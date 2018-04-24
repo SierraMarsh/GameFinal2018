1fbaa0a6efd03927eb45ba8ae561aad592c31987
SierraMarsh <ierr.mar@gmail.com>
2018-04-23 18:01:28 -0600

    Side Scroller

---
+++ GameFinal2018/Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+	public float fallDelay = 4f;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D (Collision other)
+	{
+		if (other.gameObject.CompareTag("Player")
+		rb2d.isKinematic = false;
+	}
+}
